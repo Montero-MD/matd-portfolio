8ec3d8d1805d152354c547e48d2907d5b25168b5
Montero-MD <matthewdavid.montero@wvsu.edu.ph>
2025-02-27 01:08:54 +0800

    replaced chronark's personal information with mine

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,19 +6,19 @@ import { Analytics } from "./components/analytics";
 
 export const metadata: Metadata = {
   title: {
-    default: "chronark.com",
-    template: "%s | chronark.com",
+    default: "Matthew Montero",
+    template: "%s | matd.vercel.app",
   },
-  description: "Co-founder of unkey.dev and founder of planetfall.io",
+  description: "A Software Engineer driven by results and in hot pursuit of excellence.",
   openGraph: {
-    title: "chronark.com",
+    title: "Matthew Montero",
     description:
-      "Co-founder of unkey.dev and founder of planetfall.io",
-    url: "https://chronark.com",
-    siteName: "chronark.com",
+      "A Software Engineer driven by results and in hot pursuit of excellence.",
+    url: "https://matd.vercel.app",
+    siteName: "Matthew Montero | Software Engineer",
     images: [
       {
-        url: "https://chronark.com/og.png",
+        url: "https://matd.vercel.app/og.png",
         width: 1920,
         height: 1080,
       },
@@ -36,10 +36,6 @@ export const metadata: Metadata = {
       "max-image-preview": "large",
       "max-snippet": -1,
     },
-  },
-  twitter: {
-    title: "Chronark",
-    card: "summary_large_image",
   },
   icons: {
     shortcut: "/favicon.png",
