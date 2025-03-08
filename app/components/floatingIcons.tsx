f711e16e6dc47878b2609a8435e75167966ad7c2
Montero-MD <matthewdavid.montero@wvsu.edu.ph>
2025-03-08 15:51:04 +0800

    style: placed icons on the bottom viewport

--- app/components/floatingIcons.tsx
+++ app/components/floatingIcons.tsx
@@ -2,33 +2,14 @@
 "use client";
 import React from "react";
 
-// Helper to get a random position within a quadrant
-function getRandomPositionForQuadrant(quadrant: { topMin: number; topMax: number; leftMin: number; leftMax: number }) {
-    const top = Math.floor(Math.random() * (quadrant.topMax - quadrant.topMin)) + quadrant.topMin;
-    const left = Math.floor(Math.random() * (quadrant.leftMax - quadrant.leftMin)) + quadrant.leftMin;
-    return { top, left };
-}
 
 const FloatingIcons = () => {
     const icons = [
-        { name: "🏆 Next.js is my go-to framework.", src: "/nextjs.svg" },
+        { name: "🏆 Next.js is my go-to framework for web-apps.", src: "/nextjs.svg" },
         { name: "🌐 A TypeScript Warrior, me.", src: "/typescript.svg" },
         { name: "🤖 I do Machine Learning with Python!", src: "/python.svg" },
         { name: "📱 I build apps with Flutter!", src: "/flutter.svg" },
     ];
-
-    const quadrants = [
-        { topMin: 5, topMax: 30, leftMin: 5, leftMax: 30 },   // top-left
-        { topMin: 5, topMax: 30, leftMin: 70, leftMax: 95 },  // top-right
-        { topMin: 70, topMax: 95, leftMin: 5, leftMax: 30 },  // bottom-left
-        { topMin: 70, topMax: 95, leftMin: 70, leftMax: 95 }, // bottom-right
-    ];
-
-
-    const [positions] = React.useState(() =>
-        icons.map((_, idx) => getRandomPositionForQuadrant(quadrants[idx]))
-    );
-
     const [toastMsg, setToastMsg] = React.useState<string | null>(null);
     const handleIconClick = (iconName: string) => {
         setToastMsg(iconName);
@@ -44,22 +25,22 @@ const FloatingIcons = () => {
                     {toastMsg}
                 </div>
             )}
-            {icons.map((icon, idx) => {
-                const { top, left } = positions[idx];
-                return (
+            <div className="absolute bottom-8 left-0 right-0 flex justify-center items-center gap-32 animate-fade-in">
+
+                {icons.map((icon, idx) => (
                     <div
                         key={idx}
+
                         onClick={() => handleIconClick(icon.name)}
-                        style={{ top: `${top}%`, left: `${left}%` }}
-                        className="absolute w-12 h-12 cursor-pointer transition-transform 
+                        className="w-12 h-12 cursor-pointer transition-transform 
                        duration-500 hover:scale-125 animate-fade-in"
                     >
                         <div className="w-full h-full animate-float">
                             <img src={icon.src} alt={icon.name} className="w-full h-full" />
                         </div>
                     </div>
-                );
-            })}
+                ))}
+            </div>
         </>
     );
 };
